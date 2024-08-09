920a21fc36ae543f7eeff00e6bc86d5e763fa5bf
Piotr Podusowski <podusowski@gmail.com>
2024-08-09 17:23:35 +0200

    mount current directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    ffi::{OsStr, OsString},
+    process::ExitStatus,
+};
+
 use anyhow::Context;
 use serde::Deserialize;
 
@@ -13,15 +18,35 @@ impl Recipe {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let recipe = Recipe::from_file("cio.yaml").with_context(|| "could not load cio.yaml")?;
+/// Format a value for Docker's `--volume` argument.
+fn volume_value(source: &OsStr, destination: &OsStr) -> OsString {
+    let mut value = OsString::with_capacity(source.len() + destination.len() + 1);
+    value.push(source);
+    value.push(":");
+    value.push(destination);
+    value
+}
 
-    let status = std::process::Command::new("docker")
-        .args(["run", "--rm", &recipe.image])
+fn run_in_docker(recipe: Recipe) -> Result<ExitStatus, std::io::Error> {
+    let current_dir = std::env::current_dir()?;
+    Ok(std::process::Command::new("docker")
+        .arg("run")
+        .arg("--rm")
+        // Mount current directory with the same path.
+        .args([
+            OsStr::new("--volume"),
+            &volume_value(current_dir.as_os_str(), current_dir.as_os_str()),
+        ])
+        .args([OsStr::new("--workdir"), current_dir.as_os_str()])
+        .arg(&recipe.image)
         .args(recipe.command.split_whitespace())
         .spawn()?
-        .wait()?;
+        .wait()?)
+}
 
+fn main() -> anyhow::Result<()> {
+    let recipe = Recipe::from_file("cio.yaml").with_context(|| "could not load cio.yaml")?;
+    let status = run_in_docker(recipe)?;
     println!("{:?}", status);
 
     Ok(())
