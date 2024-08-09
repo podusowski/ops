c3384e6df816582e5bb433329a70b8e90e54e1d4
Piotr Podusowski <podusowski@gmail.com>
2024-08-09 15:23:16 +0200

    trying to read recipe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct Recipe {
+    image: String,
+    command: String,
+}
+
+impl Recipe {
+    fn from_file(path: &str) -> anyhow::Result<Self> {
+        Ok(serde_yaml::from_reader(std::fs::File::open(path)?)?)
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let recipe = Recipe::from_file("cio.yaml").with_context(|| "could not load cio.yaml")?;
+
+    let docker = std::process::Command::new("docker")
+        .args(["run"])
+        .spawn()?
+        .wait()?;
+
+    println!("{:?}", docker);
+
+    Ok(())
 }
