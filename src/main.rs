f3eb487ba2e65b6bce15c4e38a3323c973af7713
Piotr Podusowski <podusowski@gmail.com>
2024-08-09 21:48:25 +0200

    run mod

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-use std::{
-    ffi::{OsStr, OsString},
-    io::Write,
-    process::{ExitStatus, Stdio},
-};
+mod run;
 
 use anyhow::Context;
+use run::run_in_docker;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
-struct Recipe {
+pub struct Recipe {
     image: String,
     script: String,
 }
@@ -17,43 +14,6 @@ impl Recipe {
     fn from_file(path: &str) -> anyhow::Result<Self> {
         Ok(serde_yaml::from_reader(std::fs::File::open(path)?)?)
     }
-}
-
-/// Format a value for Docker's `--volume` argument.
-fn volume_value(source: &OsStr, destination: &OsStr) -> OsString {
-    let mut value = OsString::with_capacity(source.len() + destination.len() + 1);
-    value.push(source);
-    value.push(":");
-    value.push(destination);
-    value
-}
-
-fn run_in_docker(recipe: Recipe) -> Result<ExitStatus, anyhow::Error> {
-    let current_dir = std::env::current_dir()?;
-
-    // https://docs.docker.com/reference/cli/docker/container/run/
-    let mut docker = std::process::Command::new("docker")
-        .arg("run")
-        .arg("--rm")
-        // Mount current directory with the same path.
-        .args([
-            OsStr::new("--volume"),
-            &volume_value(current_dir.as_os_str(), current_dir.as_os_str()),
-        ])
-        .args([OsStr::new("--workdir"), current_dir.as_os_str()])
-        // Script will be piped via stdin.
-        .arg("--interactive")
-        .stdin(Stdio::piped())
-        .arg(&recipe.image)
-        .spawn()?;
-
-    docker
-        .stdin
-        .take()
-        .ok_or(anyhow::anyhow!("cannot access Docker stdin handle"))?
-        .write_all(recipe.script.as_bytes())?;
-
-    Ok(docker.wait()?)
 }
 
 fn main() -> anyhow::Result<()> {
---
+++ src/run.rs
@@ -0,0 +1,44 @@
+use std::{
+    ffi::{OsStr, OsString},
+    io::Write,
+    process::{ExitStatus, Stdio},
+};
+
+use crate::Recipe;
+
+/// Format a value for Docker's `--volume` argument.
+fn volume_value(source: &OsStr, destination: &OsStr) -> OsString {
+    let mut value = OsString::with_capacity(source.len() + destination.len() + 1);
+    value.push(source);
+    value.push(":");
+    value.push(destination);
+    value
+}
+
+pub fn run_in_docker(recipe: Recipe) -> Result<ExitStatus, anyhow::Error> {
+    let current_dir = std::env::current_dir()?;
+
+    // https://docs.docker.com/reference/cli/docker/container/run/
+    let mut docker = std::process::Command::new("docker")
+        .arg("run")
+        .arg("--rm")
+        // Mount current directory with the same path.
+        .args([
+            OsStr::new("--volume"),
+            &volume_value(current_dir.as_os_str(), current_dir.as_os_str()),
+        ])
+        .args([OsStr::new("--workdir"), current_dir.as_os_str()])
+        // Script will be piped via stdin.
+        .arg("--interactive")
+        .stdin(Stdio::piped())
+        .arg(&recipe.image)
+        .spawn()?;
+
+    docker
+        .stdin
+        .take()
+        .ok_or(anyhow::anyhow!("cannot access Docker stdin handle"))?
+        .write_all(recipe.script.as_bytes())?;
+
+    Ok(docker.wait()?)
+}
