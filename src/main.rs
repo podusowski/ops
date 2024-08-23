9342fa34bd38c77c496f940febaae236dfeeff4d
Piotr Podusowski <podusowski@gmail.com>
2024-08-23 21:05:16 +0200

    arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,53 @@
 mod plan;
 mod run;
 
+use clap::{Parser, Subcommand};
 use plan::Plan;
 use run::run_in_docker;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None, infer_subcommands = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Enter the shell.
+    Shell,
+
+    /// Execute all missions.
+    Execute,
+}
+
 fn main() -> anyhow::Result<()> {
-    let recipe = Plan::from_file("cio.yaml")?;
-    let mut absolute_success = true;
+    let cli = Cli::parse();
 
-    for (name, mission) in recipe.missions {
-        println!("Launching '{}'", name);
-        let status = run_in_docker(mission)?;
+    match cli.command {
+        Commands::Execute => {
+            let recipe = Plan::from_file("cio.yaml")?;
+            let mut absolute_success = true;
 
-        if !status.success() {
-            println!("Mission '{}' failed with status: {:?}", name, status.code());
-            absolute_success = false;
+            for (name, mission) in recipe.missions {
+                println!("Launching '{}'", name);
+                let status = run_in_docker(mission)?;
+
+                if !status.success() {
+                    println!("Mission '{}' failed with status: {:?}", name, status.code());
+                    absolute_success = false;
+                }
+            }
+
+            if absolute_success {
+                Ok(())
+            } else {
+                Err(anyhow::anyhow!("Some missions have failed."))
+            }
         }
-    }
 
-    if absolute_success {
-        Ok(())
-    } else {
-        Err(anyhow::anyhow!("Some missions have failed."))
+        Commands::Shell => {
+            todo!();
+        }
     }
 }
