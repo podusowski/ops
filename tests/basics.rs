34a39ea7fdfc354af26e6c79208a0930033a1401
Piotr Podusowski <podusowski@gmail.com>
2024-08-24 17:23:42 +0200

    TemporaryWorkspace

--- tests/basics.rs
+++ tests/basics.rs
@@ -1,16 +1,40 @@
-use std::process::Command;
+use std::{io::Write, path::PathBuf, process::Command};
+
+struct TemporaryWorkspace(pub PathBuf);
+
+impl TemporaryWorkspace {
+    fn new(ops_yaml: &str) -> Self {
+        let dir: PathBuf = format!("/var/tmp/{}", uuid::Uuid::new_v4()).into();
+        std::fs::create_dir_all(&dir).unwrap();
+        std::fs::File::create(dir.join("cio.yaml"))
+            .unwrap()
+            .write_all(ops_yaml.as_bytes())
+            .unwrap();
+        Self(dir)
+    }
+}
+
+impl Drop for TemporaryWorkspace {
+    fn drop(&mut self) {
+        std::fs::remove_dir_all(&self.0).unwrap();
+    }
+}
 
 #[test]
 fn hello_world() {
+    let workspace = TemporaryWorkspace::new(
+        "
+        missions:
+        hello-world:
+            image: busybox
+            script: echo hello world",
+    );
+
     let program = env!("CARGO_BIN_EXE_cio");
-    let workspaces = std::path::Path::new(file!())
-        .parent()
-        .unwrap()
-        .join("workspaces");
 
     let success = Command::new(program)
         .arg("execute")
-        .current_dir(workspaces.join("hello_world"))
+        .current_dir(&workspace.0)
         .spawn()
         .unwrap()
         .wait()
